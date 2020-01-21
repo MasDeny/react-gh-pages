1c65dec32f4e6d783bc37cf372ff8fd5edf58591
Deny <kickdeny@gmail.com>
2020-01-21 10:24:20 +0700

    Updates

---
+++ precache-manifest.f7870b4047306a312b1f4f68900e76e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8824ffacb59966e2cb83886bf0d17d13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c9435851abb751ba9fe",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5d4b68956f25b772b32e",
+    "url": "/static/js/2.b8e10f1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b8e10f1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c9435851abb751ba9fe",
+    "url": "/static/js/main.3aab4afa.chunk.js"
+  },
+  {
+    "revision": "9ff75f27bff218ea1094",
+    "url": "/static/js/runtime-main.f2cc6a7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
